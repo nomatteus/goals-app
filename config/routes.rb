e2c0f18f6a33e52f0a1ee20861a04f5add726072
Matthew Ruten <matt@ruten.ca>
2011-11-22 23:36:15 -0500

    Add default root and delete public/index.html default file

--- config/routes.rb
+++ config/routes.rb
@@ -68,7 +68,7 @@ App::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'goal#index'
 
   # See how all your routes lay out with "rake routes"
 
